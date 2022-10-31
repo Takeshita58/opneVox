4371e3c926cd964bb9e1652bc36de28f2835aa3f
Takeshita <h.takeshita@varnux.com>
2022-10-31 13:19:08 +0900

    add

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const abi = [{
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ALCHEMY_HTTP))
 const contract = new web3.eth.Contract(abi,'0x2F35783908cBda09e715608824D097fe2b50a4df')
 /*const { db } = await connectToDatabase();
-let _obj = await db.collection("count").find({ totalsupply: _address}).toArray();
-const app = express()*/
+let _obj = await db.collection("count").find({ totalsupply: _address}).toArray();*/
+const app = express()
 app.get('/watch',async(req, res) => {
     console.log("watch OpneVox!")
     const _totalsupply = await contract.methods.totalSupply().call();
